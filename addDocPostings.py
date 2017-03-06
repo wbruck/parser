a96b22500637e5d3716b0f5d936bc6477621d841
Wolf <brucknerw@gmail.com>
2017-03-06 15:39:19 -0500

    merging postings lists

--- addDocPostings.py
+++ addDocPostings.py
@@ -59,7 +59,7 @@ class invertedIndex(object):
             except:
                 self.termPosting[k] = [postTuple]
 
-    def queryIndex(self,query):
+    def queryIndex(self, query):
         """query Index based on list of stemmed words"""
         queryDocs = {}
         for word in query:
@@ -72,10 +72,82 @@ class invertedIndex(object):
                 except:
                     queryDocs[docPos[0]] = positions
         print(queryDocs)
+        return queryDocs
+
+    def retrieveBestDocs(self, queryDocs, wordProximity):
+        """ranke the top X docs from query return based on wordProximity (number of words in spaces for wordProx)"""
+        docProximityScores = {}
+        for docKey, wordOccurrences in queryDocs.items():
+            lastWordPos = 0
+            docScore = 0
+
+            for i, position in enumerate(sorted(wordOccurrences)):
+                # improve this later with map
+                if i == 0:
+                    lastWordPos = position
+                    continue
+
+                if position - lastWordPos <= wordProximity:
+                    docScore += wordProximity - (position - lastWordPos)
+
+                lastWordPos = position
+
+            docProximityScores[docKey] = docScore
+
+        print (docProximityScores)
+        return docProximityScores
+
+    def checkPostingPositions(self, docId1, docId2, wordProximity):
+
+        one = [1, 4, 6, 9, 19, 20]
+        two = [3, 7, 10, 16]
+        if len(docId1) < len(docId2):
+            tempList = docId1
+            docId1 = docId2
+            docId2 = tempList
+
+        i = 0
+
+        list = []
+        for post1 in docId1:
+            for post2 in docId2[i:]:
+                if post1 > post2:
+                    list.append(post2)
+                    i += 1
+
+                elif post1 < post2:
+                    list.append(post1)
+
+                    break
+
+        list += docId2[i:]
+
+        print(list)
+
+    def getTextPositionOfDoc(self, docId, queryDocs, termDistance):
+        """get the main text segmets of the document that may contain the answer"""
+        positionsList = []
+        for i, position in enumerate(queryDocs[docId]):
+            if i == 0:
+                lastPosition = position
+                continue
+
+            if lastPosition - position < termDistance:
+                positionsList.append(position)
+
+            lastPosition = position
+
+        print(positionsList)
+
+
 
 if __name__ == "__main__":
-    one = ['1', 'spray', '5quart', 'slow', 'cooker', 'with', 'cook', 'spray']
+    #one = ['1', 'spray', '5quart', 'slow', 'cooker', 'with', 'cook', 'spray']
 
     tester = invertedIndex()
 
-    tester.indexDocument(one)
+    #tester.indexDocument(one)
+
+    one = [1, 4, 6, 9, 19, 20]
+    two = [3, 7, 10, 15]
+    tester.checkPostingPositions(one, two, 3)
--- questionText.py
+++ questionText.py
@@ -1,11 +1,19 @@
 import nltk
 import string
 import re
+from path import path
 
 import addDocPostings
 
+def readFile(fileName):
+    with open(fileName) as f:
+        s = f.read()
+    print s
+    return s
 
-one = '1. Spray 5-quart slow cooker with cooking spray. In large bowl, mix melted butter, Worcestershire sauce, seasoned salt and garlic. Add chicken; toss to coat. Pour mixture into slow cooker.'
+one = path('C:\Users/admin/Documents/575/parser/Question_Answer_Dataset_v1.2/Question_Answer_Dataset_v1.2/S08/data/set1/a1.txt.clean').bytes()
+print(one[:1000])
+#one = '1. Spray 5-quart slow cooker with cooking spray. In large bowl, mix melted butter, Worcestershire sauce, seasoned salt and garlic. Add chicken; toss to coat. Pour mixture into slow cooker.'
 two = '2 In same bowl, mix tomatoes, soup and chiles; pour over chicken.'
 three = '3 Cover; cook on High heat setting 2 to 3 hours or on Low heat setting 3 to 4 hours or until instant-read thermometer inserted in thickest part of chicken reads at least 165F.'
 four = '4 Remove chicken from slow cooker, and transfer to cutting board; let stand 5 minutes or until cool enough to handle. Meanwhile, stir cream cheese and Cheddar cheese into slow cooker. Cover; cook on High heat setting 5 to 10 minutes or until cheese melts. Stir.'
@@ -26,7 +34,7 @@ for step in steps:
     tester.indexDocument(stemmed)
 
 
-docQuery = tester.queryIndex(['add', 'bowl', 'chicken'])
+docQuery = tester.queryIndex(['kangaroo', 'european', 'settlement'])
 
 print(docQuery)
 
